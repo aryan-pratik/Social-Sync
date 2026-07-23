b704b597c0e9ffa9d8fa7cb1b3a345f0b4ad0af6
aryan-pratik <aryanpratik2004@gmail.com>
2026-07-23 08:42:26 +0530

    Added Account Connection Page

---
+++ client/src/pages/Account.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react"
+import { dummyAccountsData, PLATFORMS } from "../assets/assets"
+import { PlusIcon } from "lucide-react"
+import AccountList from "../components/AccountList"
+import PlatformPickerModal from "../components/PlatformPickerModal"
+
+const Account = () => {
+
+  const [accounts, setAccounts] = useState<any[]>([])
+  const [connecting, setConnecting] = useState<string | null[]>(null)
+  const [showPlatformPicker, setShowPlatfromPicker] = useState(false)
+
+  const fetchAccounts = async (isSync = false, platfroms: string | null, successMsg?: string) => {
+    setAccounts(dummyAccountsData);
+    console.log(isSync, platfroms, successMsg)
+  }
+
+  useEffect(() => {
+    fetchAccounts()
+  }, [])
+
+  const handleConnect = async (platformId: string ) => {
+    setConnecting([platformId])
+    setTimeout(() => {
+      setConnecting(null)
+      setAccounts((prev) => [...prev, dummyAccountsData[0]])
+      setShowPlatfromPicker(false)
+    }, 1000);
+  }
+
+  const handleDisconnect = async (accountId: string) => {
+    setAccounts(accounts.filter((a) => (a._id || a.id) !== accountId))
+  }
+  
+  const connectedIds = accounts.map((a) => a.platform)
+
+  
+  
+  return (
+    <div className="space-y-8 max-w-4xl">
+
+    {/* Header */}
+    <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4 text-sm">
+      <div>
+        <h2 className="text-xl text-slate-900">Connected Accounts</h2>
+        <p className="text-slate-500 text-sm mt-0.5">{accounts.length} of {PLATFORMS.length} platforms connected</p>
+      </div>
+      <button onClick={() => {
+        setShowPlatfromPicker(true) 
+      }} className="flex items-center gap-2 px-5 py-2.5 bg-red-500 hover:bg-red-600 text-white rounded-full font-medium w-full sm:w-auto justify-center">
+        <PlusIcon className="size-4"/> Connect Account
+      </button>
+    </div>
+
+    {/* Platfrom picker modal */}
+    {showPlatformPicker && <PlatformPickerModal connectedIds={connectedIds} connecting={connecting} onClose={()=>setShowPlatfromPicker(false)} onConnect={handleConnect}/>}
+
+    {/* Connected accounts list */}
+      <AccountList accounts={accounts} onDisconnect={handleDisconnect}/>
+
+    </div>
+  )
+}
+
+export default Account
---
+++ client/src/pages/Scheduler.tsx
@@ -0,0 +1,174 @@
+import { useEffect, useState } from "react"
+import { dummyAccountsData, dummyPostsData, PLATFORMS } from "../assets/assets";
+import { ArrowRightIcon, CalendarDaysIcon, CalendarIcon, ClockIcon, XIcon } from "lucide-react";
+
+
+const Scheduler = () => {
+  const [post, setPosts] = useState<any[]>([])
+  const [content, setContent] = useState("")
+  const [scheduleDate, setSchduleDate] = useState("");
+  const [scheduleTime, setScheduleTime] = useState("");
+  const [selectedPlatforms, setSelectedPlatforms] = useState<string[]>([]);
+  const [mediafile, setMediafile] = useState<File | null> (null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchPosts = async () => {
+    setPosts(dummyAccountsData)
+  }
+
+  useEffect( () => {
+    (async () => await fetchPosts())
+    const interval = setInterval(async () => await fetchPosts(), 20000)
+    return () => clearInterval(interval)
+  }, [])
+
+  const scheduled = post.filter((p) => p.status === 'scheduled')
+  const published = post.filter((p) => p.status === 'published')
+
+  const togglePlatform = (id: string) => {
+    setSelectedPlatforms((prev) =>
+      prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id]
+    );
+  };
+
+  const handleSchedule = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+      setPosts((prev) => [...prev, dummyPostsData[0]])
+    }, 1000)
+
+
+  }
+
+  return (
+    <div className="flex flex-col lg:flex-row gap-6 h-full">
+      {/* Compose panel */}
+      <div className="w-full lg:w-[460px] shrink-0">
+        <div className="flex items-center gap-2 mb-6">
+          <h2 className="text-lg text-slate-700">Compose Post</h2>
+        </div>
+
+        <form className="space-y-5" onSubmit={handleSchedule}>
+          {/* Platform */}
+          <div>
+            <label className="block text-xs text-slate-500 uppercase mb-2">Platforms</label>
+            <div className="flex flex-wrap gap-3">
+              {PLATFORMS.map((p) => {
+                const active = selectedPlatforms.includes(p.id);
+                return (
+                  <button key={p.id} type="button"
+                    onClick={() => togglePlatform(p.id)}
+                    className= {`flex items-center gap-1.5 p-3 rounded-md border transition-all duration-150 ${active ? "bg-red-50 border-red-300 text-red-500 scale-103" : "border-slate-200 text-slate-500 hover:border-slate-300"}`}>
+                    <p.icon className="size-4.5" />
+                  </button>
+                )
+              })}
+            </div>
+          </div>
+
+          {/* Content */}
+          <div>
+            <label className="block text-xs text-slate-500 uppercase mb-2">Content</label>
+            <textarea required rows={5} placeholder="What do you want to share today"
+            className="w-full px-5 py-4 bg-slate-50 border border-slate-200 rounded-2xl text-shadow-slate-900 placeholder-slate-400 outline-none resize-none" value={content} onChange={(e) => setContent(e.target.value)}/>
+            <div className={`text-right text-xs mt-1 font-medium ${content.length > 270 ? "text-red-500" : "text-slate-400"}`}>
+              {content.length}/270
+            </div>
+          </div>
+
+          {/* Media Upload  */}
+              <label className="block text-xs text-slate-500 uppercase mb-2">Media (optional)</label>
+              {mediafile ? (
+                <div className="realtive rounded-xl overflow-hidden border border-slate-200 bg-slate-50">
+                  {mediafile.type.startsWith("image/") 
+                  ? 
+                  <img src={URL.createObjectURL(mediafile)} alt="preview" className="w-full h-40 object-cover"/>
+                  : 
+                  <video src={URL.createObjectURL(mediafile)} className="w-full h-40 object-cover"/>} 
+
+                  <button type="button" onClick={() => setMediafile(null)} className="absolute top-2 right-2 size-7 bg-slate-900/60 hover:bg-slate-900/80 text-white rounded-full flex items-center justify-center transition-colors">
+                    <XIcon className="size-3.5"/>
+                  </button>
+                </div>
+              ) : (
+                <label className="flex items-center justify-center gap-2 p-5 py-10 border-2 border-dotted border-slate-200 rounded-xl cursor-pointer hover:border-red-300 hover:bg-red-50/30 transition-all group">
+                  <span className="text-sm text-slate-500 group-hover:text-red-600 transition-colors">Click to upload image or video</span>
+                  <input type="file" accept="image/*, video/*" className="hidden" onChange={(e) => e.target.files?.[0] && setMediafile(e.target.files[0])}/>
+                </label>
+              )}
+          {/* Date & Time  */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
+            {/* Date */}
+            <div>
+              <label className="block text-xs text-slate-500 uppercase mb-2">Date</label>
+              <div className="relative">
+                <CalendarIcon className="size-4 absolute left-3 top-1/2 -translate-y-1/2 text-slate-400 pointer-events-none"/>
+                <input type="date" required className="w-full pl-10 pr-4 py-2.5 bg-slate-50 border border-slate-200 rounded-lg text-slate-900 text-sm outline-none" value={scheduleDate} onChange={(e) => setSchduleDate(e.target.value)} />
+              </div>  
+            </div>
+
+            {/* Time */}
+            <div>
+              <label className="block text-xs text-slate-500 uppercase mb-2">Date</label>
+              <div className="relative">
+                <ClockIcon className="size-4 absolute left-3 top-1/2 -translate-y-1/2 text-slate-400 pointer-events-none"/>
+                <input type="time" required className="w-full pl-10 pr-4 py-2.5 bg-slate-50 border border-slate-200 rounded-lg text-slate-900 text-sm outline-none" value={scheduleTime} onChange={(e) => setScheduleTime(e.target.value)} />
+              </div>  
+            </div>
+          </div>
+
+          {/* Submit */}
+          <button type="submit" disabled={loading} className="w-full flex items-center justify-center gap-2 py-3.5 bg-red-500 hover:bg-red-600 transition-all text-white rounded-lg cursor-pointer">
+            {loading ? (
+              <>
+                <div className="size-4 border-2 border-white border-t-transparent rounded-full animate-spin"/> Scheduling....
+              </>
+            ) : (
+              <>
+                <span>Schedule Post</span>
+                <ArrowRightIcon className="size-4"/>
+              </>
+            )}
+          </button>
+        </form>
+      </div>
+
+      {/* Queue Panel */}
+      <div className="flex-1 flex flex-col gap-6 min-w-0">
+
+        {/* Upcoming */}
+        <div className="bg-white rounded-2xl border border-slate-200 overflow-hidden">
+          <div className="flex items-center gap-2.5 px-5 py-4 border-b border-slate-100">
+            <CalendarDaysIcon className="size-4 text-zinc-400"/>
+            <h3 className="text-slate-900 text-sm font-medium">Upcoming</h3>
+            <span className="ml-auto text-xs font-bold bg-zinc-100 text-zinc-700 px-2 py-0.5 rounded-full">{scheduled.length}</span>
+          </div>
+          <div className="max-h-72 overflow-y-auto divide-y divide-slate-50">
+            {scheduled.length === 0 ? ( 
+              <div className="py-10 text-center text-slate-400 text-sm">No posts scheduled yet</div>
+            ) : (
+              scheduled.map((postItem: any) => (
+                <div key={postItem._id || postItem.id} className="px-5 py-4 hover:bg-slate-50/60 transition-colors">
+                  <div className="flex items-center justify-between mb-2">
+                    <div>
+                      {postItem.platform.map((p1: string) => {
+                        const meta = PLATFORMS.find((p) => p.id === p1);
+                        return meta ? <meta.icon key={p1} className="size-3.5 text-slate-400"/> : null
+                      })}
+                    </div>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default Scheduler

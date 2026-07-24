617edf8a076974a72fcbe2ff141d4308e0f98e13
aryan-pratik <aryanpratik2004@gmail.com>
2026-07-24 09:12:41 +0530

    Added Scheduler service

---
+++ server/controllers/activityController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { ActivityLog } from "../models/ActivityLog.js";
+import { AuthRequest } from "../middlewares/authMiddleware.js";
+
+
+// Get all activity
+// GET /api/activity
+export const getActivity = async(req: AuthRequest, res: Response): Promise<void> => {
+    try {
+        const activity = await ActivityLog.find({
+            user: req.user._id
+        }).sort({createdAt: -1}).limit(10).populate("relatedPost", "content");
+        res.json({activity});
+    } catch(error: any) {
+        res.status(500).json({ message: error?.message || "Server error"})
+    }
+}
